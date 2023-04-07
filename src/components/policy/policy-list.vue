ece82e0a075979e4cee5d21de6ecf608c8bcb42e
garcia-alejandro <alejandro@hashed.io>
2023-04-07 16:08:23 -0600

    Modified the Policy list component to render the policies

---
+++ src/components/policy/policy-list.vue
@@ -0,0 +1,35 @@
+<template lang='pug'>
+q-card.full-width
+  .text-h5.q-pa-md Policies
+  .row(v-for="(policy,index) in policies")
+    q-item.col-12(clickable @click="onSubmit(policy)")
+      q-item-section
+        q-item-label
+          | {{ policy.name }}
+        q-item-label(caption)
+          | {{ policy.description }}
+</template>
+<script setup>
+import {
+  defineProps,
+  defineEmits,
+  toRefs,
+  toRaw
+} from 'vue'
+// props
+const props = defineProps({
+  policies: {
+    type: Array,
+    default: () => []
+  }
+})
+const { policies } = toRefs(props)
+// Emits
+const emits = defineEmits(['onSubmit'])
+// Methods by Feature
+const onSubmit = (policy) => {
+  emits('onSubmit', toRaw(policy))
+}
+</script>
+<style lang='stylus' scoped>
+</style>
